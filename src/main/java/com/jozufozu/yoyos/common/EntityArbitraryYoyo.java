40397d88fad21e71d0dbf224362e6c8d4e367957
JozsefA <jozsefaug@gmail.com>
2018-07-09 15:20:02 -0700

    Add licence to all source files Use ItemHandlerHelper instead of custom function

--- src/main/java/com/jozufozu/yoyos/common/EntityArbitraryYoyo.java
+++ src/main/java/com/jozufozu/yoyos/common/EntityArbitraryYoyo.java
@@ -1,10 +1,31 @@
+/*
+ * Copyright (c) 2018 Jozsef Augusztiny
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.jozufozu.yoyos.common;
 
 import com.jozufozu.yoyos.common.yotools.YoToolData;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -14,33 +35,11 @@ public class EntityArbitraryYoyo extends EntityYoyo
     public EntityArbitraryYoyo(World world)
     {
         super(world);
-        ignoreFrustumCheck = true;
-        width = 0.25F;
-        height = 0.25F;
     }
 
     public EntityArbitraryYoyo(World world, EntityPlayer player)
     {
-        this(world);
-        thrower = player;
-
-        CASTERS.put(player, this);
-
-        yoyoStack = thrower.getHeldItemMainhand();
-
-        if (yoyoStack == ItemStack.EMPTY)
-        {
-            yoyoStack = thrower.getHeldItemOffhand();
-
-            if (yoyoStack != ItemStack.EMPTY) hand = EnumHand.OFF_HAND;
-        }
-        else hand = EnumHand.MAIN_HAND;
-
-        Vec3d handPos = getPlayerHandPos(1);
-        setPosition(handPos.x, handPos.y, handPos.z);
-
-        if (!world.getCollisionBoxes(this, getEntityBoundingBox()).isEmpty())
-            setPosition(player.posX, player.posY + player.height * 0.85, player.posZ);
+        super(world, player);
     }
 
     @Nullable
--- src/main/java/com/jozufozu/yoyos/common/EntityYoyo.java
+++ src/main/java/com/jozufozu/yoyos/common/EntityYoyo.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2018 Jozsef Augusztiny
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.jozufozu.yoyos.common;
 
 import com.jozufozu.yoyos.Yoyos;
@@ -33,6 +55,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.IThrowableEntity;
+import net.minecraftforge.items.ItemHandlerHelper;
 import slimeknights.tconstruct.library.utils.TagUtil;
 import slimeknights.tconstruct.library.utils.TinkerUtil;
 import slimeknights.tconstruct.tools.TinkerTraits;
@@ -586,28 +609,12 @@ public class EntityYoyo extends Entity implements IThrowableEntity
         {
             for (ItemStack drop : collectedDrops)
             {
-                if (drop != null && !drop.isEmpty()) // Sometimes the items are not written correctly when saving the world
-                    giveDropToPlayer(drop);
+                // Sometimes the items are not written correctly when saving the world. Almost never, though
+                if (drop != null && !drop.isEmpty())
+                    ItemHandlerHelper.giveItemToPlayer(thrower, drop);
             }
         }
         collectedDrops.clear();
-    }
-
-    private void giveDropToPlayer(ItemStack stack)
-    {
-        if (thrower.addItemStackToInventory(stack))
-        {
-            world.playSound(null, thrower.posX, thrower.posY, thrower.posZ, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.NEUTRAL, 0.2F, ((rand.nextFloat() - rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
-        }
-        else
-        {
-            EntityItem item = new EntityItem(world, posX, posY, posZ, stack);
-            item.motionX = 0;
-            item.motionY = 0;
-            item.motionZ = 0;
-            item.setPickupDelay(0);
-            world.spawnEntity(item);
-        }
     }
 
     @Override
